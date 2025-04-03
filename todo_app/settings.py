a186f1e94b96b61ac7a47947f755d00b7731b8a6
steewo <steewojeril19@gmail.com>
2025-04-03 19:57:05 +0530

    added Todo model and register model in admin

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -26,11 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-print(f'"DBUG:{SECRET_KEY}"')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == "True"   # os.getenv("DEBUG", "True") retrieves the DEBUG value as a string from .env.
-print(f'"DBUG:{DEBUG}"')
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todoweb',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
---
+++ todoweb/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  #mandatory#ensures that if a user is deleted, all their todos are also deleted
+    title = models.CharField(max_length=255)  #mandatory
+    description = models.TextField(blank=True, null=True) #optional
+    completed = models.BooleanField(default=False) 
+    created_at = models.DateTimeField(auto_now_add=True) #automatically set
+
+
+    def __str__(self):
+        return self.title    
